a9047c07b9ec2a2aafa724a45d66149db621ada2
Shawn Hopkins <shopkins@seagen.com>
2021-02-07 06:28:16 -0800

    assignment 02 submitted

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 
 namespace VendingMachine
@@ -14,7 +15,7 @@ namespace VendingMachine
         {
             // 1) Creates and instance of can rack and set maxInventory 
             // 2)  Creates an instance of PurchasePrice and sets price
-            var vendingMachine = new VendingMachine(1, 75.0m);
+            var vendingMachine = new VendingMachine(3, 75.0m);
 
             // display the PurchasePrice.Price property
             Console.WriteLine($"The price of a soda is {vendingMachine.PurchasePrice.Price} cents\n");
@@ -26,50 +27,62 @@ namespace VendingMachine
             bool isVending = true;
             while (isVending)
             {
-                Console.WriteLine($"--Soda Options--\n{string.Join('\n',vendingMachine.AvailableFlavors())} \n\n--Current inventory--");
+                Console.WriteLine($"--Current inventory--");
                 Console.ForegroundColor = ConsoleColor.Blue;
 
                 // display contents of CanRack for each Can using can.tostring override
-                foreach (Content v in vendingMachine.CanRack.Contents())
+                foreach (Content v in vendingMachine.CanRack.Contents().OrderBy(x=>(int)x.Flavor))
                 {
                     var suf = v.Amount > 1 ? "s" : string.Empty;
-                    Console.WriteLine($"{v.Amount} can{suf} of {v.Flavor} soda");
+                    Console.WriteLine($"{(int)v.Flavor}) There is {v.Amount} can{suf} of {v.Flavor} soda in the rack   ");
+
                 }
                 Console.ForegroundColor = ConsoleColor.White;
 
                 // Parse user response into enum
-                Console.Write("\nEnter a flavor: ");
-                if (Enum.TryParse<Flavor>(Console.ReadLine(), true, out Flavor soda))
+                Console.Write("\nEnter a flavor (hit q to quit): ");
+
+                var exitKey = Console.ReadKey();
+                if (exitKey.Key == ConsoleKey.Q)
                 {
-                    // If its not emtpy remove a can
-                    if (!vendingMachine.CanRack.IsEmpty(soda))
+                    Environment.Exit(0);
+                }
+                
+                var selection = exitKey.KeyChar.ToString() + Console.ReadLine();
+                if (Enum.TryParse<Flavor>(selection, true, out Flavor soda))
+                {
+                    if (Enum.IsDefined(typeof(Flavor), soda))
                     {
-                        vendingMachine.CanRack.RemoveACanOf(soda);
-                        Console.WriteLine($"Here's your can of {soda} soda");
+                        // If its not emtpy remove a can
+                        if (!vendingMachine.CanRack.IsEmpty(soda))
+                        {
+                            vendingMachine.CanRack.RemoveACanOf(soda);
+                            Console.WriteLine($"Here's your can of {soda} soda\nhit any key to continue");
+                            Console.ReadKey();
+                        }
+                        // Otherwise prompt to add a can
+                        else
+                        {
+                            Console.Write($"\n\nThe {soda} soda is empty. Do you wish to add a can (y/n)? ");
+
+                            if (Console.ReadKey().Key == ConsoleKey.Y)
+                            {
+                                vendingMachine.CanRack.AddACanOf(soda);
+                            }
+                        }
                     }
-                    // Otherwise prompt to add a can
                     else
                     {
-                        Console.Write($"\n\nThe {soda} soda is empty. Do you wish to add a can (y/n)? ");
-
-                        if (Console.ReadKey().Key == ConsoleKey.Y)
-                        {
-                            vendingMachine.CanRack.AddACanOf(soda);
-                        }
+                        Console.WriteLine("Invalid soda flavor\nhit any key to continue");
+                        Console.ReadKey();
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Invalid soda flavor\n");
+                    Console.WriteLine("Invalid soda flavor\nhit any key to continue");
+                    Console.ReadKey();
                 }
 
-                // leave or repeat
-                Console.WriteLine("\n\nHit 'q' to quit, or any other key to continue ");
-                var resp = Console.ReadKey();
-                if (resp.Key == ConsoleKey.Q)
-                {
-                    Environment.Exit(0);
-                }
                 Console.Clear();
             }
         }
